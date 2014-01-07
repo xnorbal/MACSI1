691de9e04849fc886e968079f79185af772de70f
Philippe JOULOT <philippe.joulot@laposte.net>
2014-01-07 08:37:35 +0100

    Indentation

--- addproject.php
+++ addproject.php
@@ -5,14 +5,14 @@ include("include/top.php");
 ?>
 <h2>Projets</h2>
 <form method="post" action="createproject.php">
-<label for="intitule">Intitulé :</label>
-<input type="text" name="intitule"/>
-<br />
-<label for="perimetre">Périmètre :</label>
-<textarea name="perimetre">
-</textarea>
-<br />
-<input type="submit" value="Ajouter"/>
+	<label for="intitule">Intitulé :</label>
+	<input type="text" name="intitule"/>
+	<br />
+	<label for="perimetre">Périmètre :</label>
+	<textarea name="perimetre">
+	</textarea>
+	<br />
+	<input type="submit" value="Ajouter"/>
 </form>
 <?php
 include("include/bottom.php");
