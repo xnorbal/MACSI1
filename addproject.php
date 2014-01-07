531735a4ef77e669d564592b57b34de4f566e32e
xnorbal <xavier.norbal@gmail.com>
2014-01-07 08:53:16 +0100

    Merge de fichiers
    un seul fichier pour le formulaire et le script d'ajout de projet

--- addproject.php
+++ addproject.php
@@ -1,10 +1,18 @@
 <?php
 include("include/fonctions.php");
+
+if(!empty($_POST['ajouter']))
+{
+	$mysqli = connect();
+	$res = mysqli_query($mysqli, "INSERT INTO PROJET(INTITULE, PERIMETRE) VALUES('".$_POST['intitule']."', '".$_POST['perimetre']."')");
+	header("location:projectlist.php");
+}
+
 $titre = "Ajout de projet";
 include("include/top.php");
 ?>
 <h2>Projets</h2>
-<form method="post" action="createproject.php">
+<form method="post" action="addproject.php">
 	<label for="intitule">Intitulé :</label>
 	<input type="text" name="intitule"/>
 	<br />
@@ -12,7 +20,7 @@ include("include/top.php");
 	<textarea name="perimetre">
 	</textarea>
 	<br />
-	<input type="submit" value="Ajouter"/>
+	<input type="submit" name="ajouter" value="Ajouter"/>
 </form>
 <?php
 include("include/bottom.php");
--- createproject.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mysqli = connect();
-$res = mysqli_query($mysqli, "INSERT INTO PROJET(INTITULE, PERIMETRE) VALUES('".$_POST['intitule']."', '".$_POST['perimetre']."')");
-header("location:projectlist.php");
-?>
