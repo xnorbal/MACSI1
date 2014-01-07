30d85969a69b26cf0a6ff2b1cdd076c2b1d0741d
Philippe JOULOT <philippe.joulot@laposte.net>
2014-01-07 13:39:29 +0100

    Ajout de phases (avec jalons)

--- include/fonctions.php
+++ include/fonctions.php
@@ -5,4 +5,16 @@ function connect() {
 	return $mysqli;
 }
 
+function datefrToEn($date) { 
+$split = explode("/",$date); 
+$jour = $split[0]; 
+$mois = $split[1]; 
+$annee = $split[2]; 
+return "$mois"."/"."$jour"."/"."$annee"; 
+}
+
+function formatTimestampToDateSQL($timestamp) {  
+return date("Y-m-d",$timestamp); 
+} 
+
 ?>
