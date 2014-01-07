a74648ff639d1c672f964989255f10d7893abab6
Philippe JOULOT <philippe.joulot@laposte.net>
2014-01-07 10:47:36 +0100

    Intégration JQuery

--- include/top.php
+++ include/top.php
@@ -10,7 +10,10 @@ echo $titre;
 ?>
 </title>
 <link rel="stylesheet" type="text/css" href="site.css" />
-<head>
+<link rel="stylesheet" type="text/css" href="JQuery/css/smoothness/jquery-ui-1.10.3.custom.min.css" />
+<script type="text/javascript" src="JQuery/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="JQuery/jquery-ui-1.10.3.custom.min.js"></script>
+</head>
 <body>
 <div id="content">
 <img src="images/logo.png" alt="logo" id="logo" />
