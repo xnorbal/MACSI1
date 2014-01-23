d717b27ec6efa4c038ef14ebceb3a1fb8924fd77
Philippe JOULOT <philippe.joulot@laposte.net>
2014-01-23 13:58:28 +0100

    Page d'accueil

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include("include/fonctions.php");
+$titre = "Accueil";
+
+?>
+<?php include("include/top.php"); ?>
+<h2><?php echo $titre; ?></h2>
+
+<div>
+	<h3 class="center">Bienvenue sur l'application de gestion de projets GESPRO.</h3>
+	<p class="center">
+		L'application GESPRO est un logiciel de gestion de projets développé dans le cadre du projet MACSI1 sous la direction de Mr Yehia TAHER.
+	</p>
+	<a href="https://github.com/xnorbal/MACSI1/" class="buttonFollow git">
+		Retrouvez-nous sur GitHub
+	</a>
+</div>
+
+<?php
+include("include/bottom.php");
+?>
