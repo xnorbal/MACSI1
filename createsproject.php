1ac42782b8a78a1083509e8cc45ad5888bd0173d
Chamlee <arnaud8ruckebusch@gmail.com>
2014-01-23 08:39:56 +0100

    recorrectif

--- createsproject.php
+++ createsproject.php
@@ -2,5 +2,6 @@
 include("include/fonctions.php");
 $mysqli = connect();
 $res = mysqli_query($mysqli, "INSERT INTO SOUSPROJET(PID, INTITULE, PERIMETRE) VALUES(".$_POST['pid'].", ".$_POST['intitule']." , ".$_POST['perimetre'].")");
-header("location:sprojectlist.php?pid=".$_POST['pid']);
+header("location:projectdetails.php?pid=".$_POST['pid']);
+
 ?>
